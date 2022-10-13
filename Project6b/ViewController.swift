29063b3324a3acbd33ce79bcd2ea19c47075fa31
tborysova <borysova.tetiana@gmail.com>
2022-10-13 14:13:49 +0200

    Added Auto Layout anchors

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Project6b
+//  Project6b Auto Layout anchors
 //
 //  Created by Tetiana Borysova on 11.10.22.
 //
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -50,24 +50,23 @@ class ViewController: UIViewController {
         view.addSubview(label5)
         
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3":
-                                label3, "label4": label4, "label5": label5]
-        // Do any additional setup after loading the view.
+        var previous: UILabel?
         
-        for label in viewsDictionary.keys {
-            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo:
+                                            view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previous = label
         }
-        
-        
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
-        
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label1]|", options: [], metrics: nil, views: viewsDictionary))
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label2]|", options: [], metrics: nil, views: viewsDictionary))
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label3]|", options: [], metrics: nil, views: viewsDictionary))
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label4]|", options: [], metrics: nil, views: viewsDictionary))
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label5]|", options: [], metrics: nil, views: viewsDictionary))
     }
-
-
+    
+    
 }
 
